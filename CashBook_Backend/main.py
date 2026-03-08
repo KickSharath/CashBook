7515c12fe5ceb973ed367a6cd87241460558e638
KICK Sharath <kicksharath234@gmail.com>
2026-03-08 09:44:14 +0530

    #008 - 5 - BackEnd -Fixes

--- CashBook_Backend/main.py
+++ CashBook_Backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(docs_url=None, redoc_url=None, title="Cash Book", lifespan=lifespa
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "https://incandescent-souffle-03646b.netlify.app/"],
+    allow_origins=["http://localhost:4200", "https://incandescent-souffle-03646b.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
